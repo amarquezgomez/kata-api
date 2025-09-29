62eb3d18e9692e3f3c767aa42d41f9d3b5182d64
Alejandro Marquez <a.marquezgomez@yahoo.com>
2025-09-29 22:07:35 +0200

    refactor(models): adjust CreatedBooking to match API responses

--- src/main/java/com/amgqa/kata/models/CreatedBooking.java
+++ src/main/java/com/amgqa/kata/models/CreatedBooking.java
@@ -1,6 +1,7 @@
 package com.amgqa.kata.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)  // ← Added to ignore extra fields like "error"
 public class CreatedBooking {
 
     @JsonProperty("bookingid")
